7d42c59b4e7256da662b7138a9d14e4577a71ea1
Chris Reynoso <chrtravels@gmail.com>
2023-06-24 13:24:55 -0700

    added suggestionCard & SVGR with webpack config

---
+++ src/components/suggestionCard/SuggestionCard.js
@@ -0,0 +1,28 @@
+import styles from './suggestionCard.module.scss';
+
+import CommentsIcon from '../../assets/shared/icon-comments.svg';
+
+function SuggestionCard() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftContent}>
+        <div className={styles.voteCounter}>
+
+        </div>
+
+        <div className={styles.content}>
+          <h3>Card Name</h3>
+          <p className="body1">Card body content</p>
+          <div className={styles.tag}></div>
+        </div>
+      </div>
+
+      <div className={styles.commentCount}>
+        <CommentsIcon />
+        <h4>3</h4>
+      </div>
+    </div>
+  );
+}
+
+export default SuggestionCard;
