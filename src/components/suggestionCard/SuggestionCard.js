3315962c94ac38920bdcc3d6dc25302f8646da39
Chris Reynoso <chrtravels@gmail.com>
2023-07-13 14:32:51 -0700

    add tag to SuggestionCard component & updated styling

--- src/components/suggestionCard/SuggestionCard.js
+++ src/components/suggestionCard/SuggestionCard.js
@@ -2,30 +2,36 @@ import styles from './suggestionCard.module.scss';
 
 import {ReactComponent as CommentsIcon} from '../../assets/shared/icon-comments.svg';
 import {ReactComponent as UpArrow} from '../../assets/shared/icon-arrow-up.svg';
+import { Link } from 'react-router-dom';
 
 function SuggestionCard(props) {
   const {title, upvotes, status, description, comments, category} = props;
 
+
   return (
-    <div className={styles.container}>
-      <div className={styles.leftContent}>
-        <div className={`${styles.voteCount} tag-vote`}>
-          <UpArrow />
-          <h4>{upvotes}</h4>
+    <Link style={{textDecoration: 'none'}} to='/feedback-detail'>
+      <div className={styles.container}>
+        <div className={styles.leftContent}>
+          <div className={`${styles.voteCount} tag-vote`}>
+            <UpArrow />
+            <h4>{upvotes}</h4>
+          </div>
+
+          <div className={styles.content}>
+            <h3 className='dark-font'>{title}</h3>
+            <p className="body-2 light-font">{description}</p>
+            <div className={`${styles.tagContainer} tag-vote`}>
+              <span className='tag'>{category}</span>
+            </div>
+          </div>
         </div>
 
-        <div className={styles.content}>
-          <h3 className='dark-font'>{title}</h3>
-          <p className="body-2 light-font">{description}</p>
-          <div className={styles.tag}></div>
+        <div className={styles.commentCount}>
+          <CommentsIcon />
+          <h4 id={styles.commentCount}>3</h4>
         </div>
       </div>
-
-      <div className={styles.commentCount}>
-        <CommentsIcon />
-        <h4 id={styles.commentCount}>3</h4>
-      </div>
-    </div>
+    </Link>
   );
 }
 
